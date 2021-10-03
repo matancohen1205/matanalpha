4083dd8c69f440cc3746af48bdc99ae62778becb
matan <matan145222@gmail.com>
2021-10-03 16:14:40 +0300

    updated phyton

--- GitExcrecise/program.py
+++ GitExcrecise/program.py
@@ -1,8 +1,7 @@
 def main():
     sum = 1
-    for num in range(1, 11):
-        if (num % 2 == 1):
-            sum = sum * num
+    for num in range(1, 101):
+        sum = sum + num
     print (sum)
 
 if (__name__ == "__main__"):
