44af787d6a73f78eaa0091f39cd41df07914dba8
matan <matan145222@gmail.com>
2021-10-03 16:12:40 +0300

    phyton

--- GitExcrecise/program.py
+++ GitExcrecise/program.py
@@ -1,1 +1,9 @@
-print ("Matan King")
+def main():
+    sum = 1
+    for num in range(1, 11):
+        if (num % 2 == 1):
+            sum = sum * num
+    print (sum)
+
+if (__name__ == "__main__"):
+    main()
